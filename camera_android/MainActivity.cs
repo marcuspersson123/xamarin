5ecd7e2ea996870b99089ab13dd2f42cdf771b2c
Marcus Persson <marcuspersson123@gmail.com>
2014-03-01 18:24:08 +0100

    Resize av bitmap

--- camera_android/MainActivity.cs
+++ camera_android/MainActivity.cs
@@ -14,6 +14,7 @@ using Java.IO;
 using Environment = Android.OS.Environment;
 using System.Threading;
 using MomentsApp.Core;
+using Android.Content.Res;
 
 namespace MomentsApp
 {
@@ -56,15 +57,16 @@ namespace MomentsApp
 				ThreadPool.QueueUserWorkItem (o => { 
 					MomentsManager.GetPhoto (_moment);
 
-					RunOnUiThread(() => {
-						displayMoment();
+					RunOnUiThread (() => {
+						displayMoment ();
 					});
 				}
 				);
 			}
 		}
 
-		protected override void OnDestroy() {
+		protected override void OnDestroy ()
+		{
 			if (!_isChangingOrientation) {
 				if (_moment != null && _moment.Photo != null) {
 					_moment.Photo.Recycle ();
@@ -137,13 +139,15 @@ namespace MomentsApp
 				_moment = new MomentsApp.Core.Moment ();
 
 				try {
-					Bitmap bitmap = BitmapFactory.DecodeFile (_file.Path);
+					//Bitmap bitmap = BitmapFactory.DecodeFile (_file.Path);
+					Bitmap bitmap = BitmapFromFile (_file, 100, 100);
+
 					_moment.Photo = bitmap;
 					_moment.Latitude = "1.2";
 					_moment.Longitude = "1.3";
 					_moment.Note = "test";
 					_moment.Time = "343434";
-					MomentsManager.SaveMoment(_moment);
+					MomentsManager.SaveMoment (_moment);
 				} catch (Exception e) {
 
 				}
@@ -153,6 +157,46 @@ namespace MomentsApp
 		private void SavePhotoButtonClick (object sender, EventArgs eventArgs)
 		{
 			ThreadPool.QueueUserWorkItem (o => MomentsManager.SaveMoment (_moment));
+		}
+
+		public static Bitmap BitmapFromFile (File file, int reqWidth, int reqHeight)
+		{
+
+			// First decode with inJustDecodeBounds=true to check dimensions
+			BitmapFactory.Options options = new BitmapFactory.Options ();
+			options.InJustDecodeBounds = true;
+			BitmapFactory.DecodeFile (file.Path, options);
+
+			// Calculate inSampleSize
+			options.InSampleSize = CalculateInSampleSize (options, reqWidth, reqHeight);
+
+			// Decode bitmap with inSampleSize set
+			options.InJustDecodeBounds = false;
+			return BitmapFactory.DecodeFile (file.Path, options);
+		}
+
+		public static int CalculateInSampleSize (
+			BitmapFactory.Options options, int reqWidth, int reqHeight)
+		{
+			// Raw height and width of image
+			int height = options.OutHeight;
+			int width = options.OutWidth;
+			int inSampleSize = 1;
+
+			if (height > reqHeight || width > reqWidth) {
+
+				int halfHeight = height / 2;
+				int halfWidth = width / 2;
+
+				// Calculate the largest inSampleSize value that is a power of 2 and keeps both
+				// height and width larger than the requested height and width.
+				while ((halfHeight / inSampleSize) > reqHeight
+				       && (halfWidth / inSampleSize) > reqWidth) {
+					inSampleSize *= 2;
+				}
+			}
+
+			return inSampleSize;
 		}
 	}
 }
